7313f9b6306ac493b655c851cafdf2722dbff332
asliismailov <157467611+asliismailov@users.noreply.github.com>
2024-04-14 16:43:10 +0300

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -60,41 +60,28 @@ with chart_tab:
         st.subheader("SHAP Değerleri")
         st.image("SHAP.png")
 
+
 # Tahmin sekmesi
-def predict_obesity_risk(age, gender, weight, height, ch2o):
-    # Cinsiyeti sayısal değere dönüştür
-    gender_dict = {'Male': 1, 'Female': 0}
-    gender_numeric = gender_dict[gender]
-
-    # DataFrame oluştur
-    input_data = pd.DataFrame({
-        'Age': [age],
-        'Gender': [gender_numeric],
-        'Weight': [weight],
-        'Height': [height],
-        'CH2O': [ch2o]
-    })
-
-    # Modelin tahmin yapmasını sağlayın
-    prediction = model.predict(input_data)
-    return prediction
-
-
 with prediction_tab:
     st.header("Model ile Tahmin Yapma")
     st.subheader("Tahmin")
+
+    # Kullanıcı girdileri
     selected_age = st.number_input("Yaş", min_value=0, max_value=150, value=30, step=1)
-    selected_gender = st.radio("Cinsiyet", ["Erkek", "Kadın"])
+    
+    # Cinsiyet seçimi için güncellenmiş kısım
+    gender_options = {'Erkek': 'Male', 'Kadın': 'Female'}
+    selected_gender = st.radio("Cinsiyet", list(gender_options.keys()))
+
     selected_weight = st.number_input("Kilo (kg)", min_value=20, max_value=500, value=70, step=1)
     selected_height = st.number_input("Boy (cm)", min_value=50, max_value=300, value=170, step=1)
     selected_ch2o = st.number_input("Günlük Su Tüketimi (ml)", min_value=0, max_value=10000, value=2000, step=100)
 
     if st.button("Tahminle"):
-        prediction = predict_obesity_risk(selected_age, selected_gender, selected_weight, selected_height,
-                                          selected_ch2o)
+        # Cinsiyetin sayısal değerine dönüştürülmesi
+        gender_numeric = gender_dict[gender_options[selected_gender]]
+        
+        # Tahmin fonksiyonunu çağırma
+        prediction = predict_obesity_risk(selected_age, gender_numeric, selected_weight, selected_height, selected_ch2o)
         st.write("Tahmin Edilen Obezite Riski:", prediction)
-        st.balloons()
-
-
-
 
