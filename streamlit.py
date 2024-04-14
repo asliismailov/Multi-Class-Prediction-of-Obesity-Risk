7a5f72dc165721680d8022fae2f5944904d42c29
asliismailov <157467611+asliismailov@users.noreply.github.com>
2024-04-14 17:39:31 +0300

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -38,6 +38,11 @@ def predict_obesity_risk(age, gender, weight, height, ch2o):
     prediction_proba = model.predict_proba(input_data)
     return prediction_proba
 
+# BMI hesaplama fonksiyonu
+def calculate_bmi(height, weight):
+    bmi = weight / (height ** 2)
+    return bmi
+
 # Ana Sayfa layout
 main_tab, chart_tab, prediction_tab = st.tabs(["Ana Sayfa", "Grafikler", "Model"])
 
@@ -61,6 +66,18 @@ with prediction_tab:
     with st.form(key='obesity_form'):
         selected_age = st.number_input("Yaş", min_value=0, max_value=150, value=30, step=1)
         selected_gender = st.radio("Cinsiyet", list(gender_options.keys()))
-        selected_weight = st
+        selected_weight = st.number_input("Kilo (kg)", min_value=20, max_value=500, value=70, step=1)
+        selected_height = st.number_input("Boy (cm)", min_value=50, max_value=300, value=170, step=1)
+        selected_ch2o = st.number_input("Günlük Su Tüketimi (ml)", min_value=0, max_value=10000, value=2000, step=100)
+        
+        submit_button = st.form_submit_button(label='Tahminle')  # Submit butonu eklendi
 
-
+        if submit_button:
+            # Kullanıcı seçimini sayısal değere dönüştürme
+            gender_numeric = gender_dict[gender_options[selected_gender]]
+            # BMI hesaplama
+            bmi = calculate_bmi(selected_height, selected_weight)
+            # Tahmin fonksiyonunu çağırma
+            prediction = predict_obesity_risk(selected_age, gender_numeric, selected_weight, selected_height, selected_ch2o)
+            st.write("Tahmin Edilen Obezite Riski:", prediction)
+            st.write("BMI:", bmi)
