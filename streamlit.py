090c060e99d3a9ef49c30f8fc82082085b0f26ed
asliismailov <157467611+asliismailov@users.noreply.github.com>
2024-04-17 09:43:55 +0300

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -6,17 +6,13 @@ import numpy as np
 # Modelin yüklenmesi
 model = joblib.load('lgbm_model.pkl')
 
-def display_about():
-    st.title('Hakkımızda')
-    # Hakkımızda bilgileri ve takım üyeleri detayları
-
 # Kullanıcı girişlerini al
 st.title('Obezite Durumu Tahmini')
 
 # Modelin en çok etkilendiği özellikler
-weight = st.number_input('Kilo (kg):', min_value=0.0, format="%.2f")
-age = st.number_input('Yaş:', min_value=0, max_value=120)
-height = st.number_input('Boy (metre):', min_value=0.1, format="%.2f")  # Metreye çevir, minimum değer 0.1 olarak güncellendi
+weight = st.number_input('Kilo (kg):', min_value=0.0, format="%.2f", value=70.0)
+age = st.number_input('Yaş:', min_value=0, max_value=120, value=25)
+height = st.number_input('Boy (metre):', min_value=0.1, format="%.2f", value=1.75)
 
 faf = st.slider('Fiziksel aktivite frekansı (haftada kaç gün):', 0, 7, 1)
 ch2o = st.slider('Günlük su tüketimi (Litre):', 0.0, 5.0, 1.0)
@@ -26,37 +22,25 @@ ncp = st.slider('Günlük ana öğün sayısı:', 1, 5, 3)
 gender = st.selectbox('Cinsiyet:', ['Kadın', 'Erkek'])
 family_history_with_overweight = st.selectbox('Ailede aşırı kilolu birey var mı?', ['Evet', 'Hayır'])
 
-# BMI hesaplama ve 0 bölme hatası kontrolü
-if height > 0:
-    bmi = weight / (height ** 2)
-else:
-    bmi = 0  # height 0 ise BMI 0 olarak atanabilir veya kullanıcıya hata mesajı gösterilebilir.
+# BMI hesaplama
+bmi = weight / (height ** 2)
 
-# Varsayılan ve kullanıcı girdilerinin birleştirilmesi
-default_values = {
-    "CALC_Always": 0,
-    "MTRANS_Walking": 0,
-    # Diğer varsayılan değerler
-    "BMI": bmi
-}
+# Veri çerçevesi oluşturuluyor
+input_df = pd.DataFrame({
+    'Weight': [weight],
+    'Age': [age],
+    'Height': [height],
+    'FAF': [faf],
+    'CH2O': [ch2o],
+    'TUE': [tue],
+    'FCVC': [fcvc],
+    'NCP': [ncp],
+    'Gender': [1 if gender == 'Erkek' else 0],
+    'family_history_with_overweight': [1 if family_history_with_overweight == 'Evet' else 0],
+    'BMI': [bmi]
+})
 
-user_input = {
-    'Weight': weight,
-    'Age': age,
-    'Height': height,
-    'FAF': faf,
-    'CH2O': ch2o,
-    'TUE': tue,
-    'FCVC': fcvc,
-    'NCP': ncp,
-    'Gender': 1 if gender == 'Erkek' else 0,
-    'family_history_with_overweight': 1 if family_history_with_overweight == 'Evet' else 0,
-}
-
-full_data = {**default_values, **user_input}
-
-# Veri çerçevesi oluşturuluyor ve tahmin yapılıyor
-input_df = pd.DataFrame([full_data])
+# Tahmin yapılıyor
 if st.button('Tahmin Yap'):
     prediction = model.predict(input_df)
     st.write(f'Tahmin edilen Obezite Durumu: {prediction[0]}')
